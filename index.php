8a015de41af0eeb83db1195b96a47434d1bae023
glbdmn <gabrieledamiani@hotmail.it>
2023-05-22 14:50:08 +0200

    scritta funzione per calcolo password

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="file.css/style.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+</head>
+<body>
+
+    <?php 
+        $lughezzaPassword = $_GET['psw_length'];
+
+        function getRandomPassword($lunghezzaPassword){
+          $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?&%$<>^+-*/()[]{}@#_=';
+          $password = '';
+          for($i = 0; $i < $lunghezzaPassword; $i++){
+            $password .= $caratteri[rand (0, strlen($caratteri))];
+          }
+          return $password;
+        };
+    ?>
+
+
+
+
+    <div class="container m-0">
+        <form action="index.php" method="GET" >
+            <div class="row">
+                <div class="col">
+                   <label for="input">Inserisci la lunghezza della password da generare</label>
+                   <input type="number" name="psw_length" min="1" max="20">
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <button type="submit" class="btn btn-info">Genera</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
