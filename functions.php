5d30d5046c15e3be3fc612d4b967cb86d78d0281
glbdmn <gabrieledamiani@hotmail.it>
2023-05-22 17:32:10 +0200

    bottone

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 <?php 
-        $lunghezzaPassword = $_GET['lunghezza_password'];
+         $lunghezzaPassword = $_GET['lunghezza_password'];
 
         function getRandomPassword($lunghezzaPassword){
           $password = '';
           $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?&%$<>^+-*/()[]{}@#_=';
     
           for($i = 0; $i < $lunghezzaPassword; $i++){
-            $password .= $caratteri[rand (0, strlen($caratteri))];
-          }
-        //   var_dump($password);
-          return $password;
-        };
-        // var_dump($password);
-        getRandomPassword($lunghezzaPassword);
+               $password .= $caratteri[rand (0, strlen($caratteri))];
+           }
+            // var_dump($password);
+           return $password;
+         };
+          // var_dump($password);
+        //  getRandomPassword($lunghezzaPassword);
     ?>
